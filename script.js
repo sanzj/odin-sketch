e1dd97285d50c3ae7dc4dd0f624e067231c7b780
Amelia <sanzj96@gmail.com>
2022-05-09 16:30:43 -0400

    add clear grid functionality

--- script.js
+++ script.js
@@ -2,17 +2,19 @@
 const grid = document.querySelector("#grid-container");
 
 let gridSize = 24;
-let ink = 'color';
+let ink = 'black';
 
-document.querySelector('h1').textContent = gridSize;
+document.querySelector('h2').textContent = gridSize + 'x' + gridSize;
 createGrid(gridSize);
 
+console.log(grid.offsetHeight);
 
 //INITIALIZE EVENT LISTENERS
-document.querySelector('#size-btn').addEventListener('click', e => promptSize());
-document.querySelector('#black-ink').addEventListener('click', e => ink = 'black');
-document.querySelector('#color-ink').addEventListener('click', e => ink = 'color');
-document.querySelector('#eraser').addEventListener('click', e => ink = 'eraser');
+document.querySelector('#size-btn').addEventListener('click', promptSize);
+document.querySelector('#clear-btn').addEventListener('click', shakeGrid);
+document.querySelector('#black-ink').addEventListener('click', () => ink = 'black');
+document.querySelector('#color-ink').addEventListener('click', () => ink = 'color');
+document.querySelector('#eraser').addEventListener('click', () => ink = 'eraser');
 
 //FUNCTION DECLARATIONS
 function promptSize(){
@@ -20,10 +22,11 @@ function promptSize(){
 
     while(input < 12 || input > 48){
         input = prompt('How many pixels per row (12-48)');
+        if(input === null) return;
     }
 
     gridSize = input;
-    document.querySelector('h1').textContent = gridSize;
+    document.querySelector('h2').textContent = gridSize + 'x' + gridSize;
     clearGrid();
     createGrid(input);
 }
@@ -35,7 +38,7 @@ function createGrid(size){
         square.classList.add('grid-square');
         square.addEventListener('mouseover', e => {
             if (ink == 'black') square.style.backgroundColor = 'black';
-            else if (ink == 'color') square.style.backgroundColor = 'red';
+            else if (ink == 'color') square.style.backgroundColor = '#' + Math.floor(Math.random()*16777215).toString(16);
             else if (ink == 'eraser') square.style.backgroundColor = 'white';
         })
     
@@ -46,8 +49,15 @@ function createGrid(size){
     }
 }
 
+//Deletes grid, only used when remaking right after
 function clearGrid(){
     while(grid.firstChild){
         grid.removeChild(grid.lastChild);
     }
+}
+
+//Doesnt delete the grid but makes all squares white, like shaking an etchasketch
+function shakeGrid() {
+    let elements = Array.from(grid.children);
+    elements.forEach( (el) => el.style.backgroundColor = 'white');
 }
